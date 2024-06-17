85bfc435234f709333c9a7b037dff931db2348d7
Kris-Vl <114858847+Kris-Vl@users.noreply.github.com>
2024-06-17 21:37:06 +0300

    Update server.ts

--- server.ts
+++ server.ts
@@ -2,9 +2,24 @@ const allowedUsers: string[] = [
     "steam:11000010a0b1c2d3"
 ];
 
-onNet("checkNoclipPermission", () => {
-    const source = global.source;
-    const playerID = GetPlayerIdentifiers(source)[0];
-    const isAllowed = allowedUsers.includes(playerID);
-    emitNet("noclip:allowed", source, isAllowed);
+onNet("checkNoclipPermission", (playerSource: number) => {
+    const playerIdentifiers: string[] = getPlayerIdentifiers(playerSource);
+    if (playerIdentifiers.length > 0) {
+        for (const id of allowedUsers) {
+            if (playerIdentifiers.includes(id)) {
+                emitNet("noclip:allowed", playerSource, true);
+                return;
+            }
+        }
+    }
+    emitNet("noclip:allowed", playerSource, false);
 });
+
+function getPlayerIdentifiers(playerSource: number): string[] {
+    const identifiers: string[] = [];
+    const numIdentifiers = GetNumPlayerIdentifiers(playerSource);
+    for (let i = 0; i < numIdentifiers; i++) {
+        identifiers.push(GetPlayerIdentifier(playerSource, i));
+    }
+    return identifiers;
+}
