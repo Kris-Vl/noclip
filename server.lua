b4a2eb506877d2c2441e7ff28b7aa11888754bf9
Kris-Vl <114858847+Kris-Vl@users.noreply.github.com>
2024-06-17 14:26:09 +0300

    Update server.lua

--- server.lua
+++ server.lua
@@ -2,15 +2,15 @@ local allowedUsers = {
     "steam:11000010a0b1c2d3"
 }
 
-RegisterServerEvent("checkPermission")
-AddEventHandler("checkPermission", function()
+RegisterServerEvent("checkNoclipPermission")
+AddEventHandler("checkNoclipPermission", function()
     local source = source
     local playerID = GetPlayerIdentifiers(source)[1]
     for _, id in pairs(allowedUsers) do
         if id == playerID then
-            TriggerClientEvent("noclip:allowed", source)
+            TriggerClientEvent("noclip:allowed", source, true)
             return
         end
     end
-    TriggerClientEvent("noclip:denied", source)
+    TriggerClientEvent("noclip:allowed", source, false)
 end)
